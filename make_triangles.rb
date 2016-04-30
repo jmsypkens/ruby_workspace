e9d66af43acb767334d2beea979f65d0c86710bc
Jerry Sypkens <sypkenjm5296@student.laccd.edu>
2016-04-30 19:35:07 +0000

    make_triangles

---
+++ make_triangles.rb
@@ -0,0 +1,16 @@
+def make_triangles(n)
+    counter = 0
+    stars = '*'
+    puts stars  
+    
+    while counter <= n 
+        puts stars = stars + "*"
+        counter += 1
+    end 
+end 
+
+p "How many rows would you like in your triangle?"
+user_entry = gets.chomp 
+int_num = user_entry.to_i
+
+make_triangles(int_num) 
